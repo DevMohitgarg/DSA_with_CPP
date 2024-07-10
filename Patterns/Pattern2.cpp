69d9e8dd2a4b6f66ff421324d2b78c0c837f3b37
Mohit <gargmohit1044@gmail.com>
2024-07-10 18:44:14 +0530

    Practicing Patterns

---
+++ Patterns/Pattern2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row,col;
+
+    for (row = 1; row <=4 ; row++)
+    {
+        for (col = 0; col <= 4; col++)
+        {
+            cout<<10<<" ";
+        }
+        cout<<endl;        
+    }
+    
+}
