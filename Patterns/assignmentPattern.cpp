0a16c8fac736faba76331e403ecbb29f6327e0e0
Mohit <gargmohit1044@gmail.com>
2024-09-22 22:31:47 +0530

    assignment pattern

---
+++ Patterns/assignmentPattern.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=0;i<5;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<< j*2;
+        }
+        cout<<endl;
+    }
+    for(int i=5;i>=1;i--)
+    {
+        for(int j=7;j>=7-i+1;j--)
+        {
+            cout<< j;
+        }
+        cout<<endl;
+    }
+ return 0;
+}
